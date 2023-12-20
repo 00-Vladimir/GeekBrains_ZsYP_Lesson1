bd328ecb60f25448eded17c54d8c271f3873afc3
VladimirSh <a1.shumilovvv@yandex.ru>
2023-12-20 19:07:20 +0500

    added bariables_Lesson1_Task1___

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, Wirld");
+﻿int volue = 100;
+Console.WriteLine(volue);
+
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
